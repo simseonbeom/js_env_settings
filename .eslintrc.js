f039dc16346c218293eaf158bb1ec203dcc5aebc
kindtiger <seonbeom2@gmail.com>
2022-07-06 14:11:22 +0900

    환경구성 완료

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+// node.js
+// commonjs module load export
+
+
+// import = require()
+// export = module.exports / exports.모듈이름
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "prettier"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+    "root":true,
+}
---
+++ server/index.js
@@ -0,0 +1,12 @@
+
+const server = require('live-server');
+
+const params = {
+  host: 'localhost',
+  port: 3000,
+  open: false,
+  root: './client',
+};
+
+
+server.start(params);
