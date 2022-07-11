8f3c821c10b669a25d09749749e9d198b0f4eae8
kindtiger <seonbeom2@gmail.com>
2022-07-11 18:14:44 +0900

    study type-conversion

---
+++ client/chapter/05.type-conversion.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/* ---------------------------------------------------------------------- */
+/* Type Conversion                                                        */
+/* ---------------------------------------------------------------------- */
+
+
+/* 데이터 → 문자 ----------------------------------------------------------- */
+
+// number -> string
+let year = 2020; // number
+
+let convertedString = year.toString();
+
+console.log(typeof convertedString);
+
+
+// undefined, null
+
+// boolean
+
+
+/* 데이터 → 숫자 ----------------------------------------------------------- */
+
+// undeinfed
+
+// null
+
+// boolean
+
+// string
+
+// numeric string
+// window.parseFloat(getComputedStyle(h1).fontSize)
+
+/* 데이터 → 불리언 ---------------------------------------------------------- */
+
+// null, undefined, 0, NaN, ''
+// 위에 나열한 것 이외의 것들 
+
+// 방법 1. Boolean()
+// 방법 2. !!dataType 
+
+
